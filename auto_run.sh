89beead42986bd84e4b8f936fd968b2f6e4a22e0
ilufei <dzxsk1988@gmail.com>
2015-10-11 21:44:57 +0800

    update auto_run.sh

--- auto_run.sh
+++ auto_run.sh
@@ -3,6 +3,7 @@ usage()
 {
 	echo "usage: `basename $0` download|rnew"
 	echo "           download <file>      download file with oflash tools"
+	echo "           new <build_path>    clean all files, remake uboot"
 	echo "           rnew <build_path>    clean all files, remake uboot, then download it"
 }
 
@@ -13,6 +14,12 @@ then
 fi
 
 case $1 in
+	new) echo "clean all temp files, remake uboot......"
+	rm -rf $2
+	make O=$2 clean
+	make CROSS_COMPILE=arm-linux- ARCH=arm O=$2 fl2440_config
+	make CROSS_COMPILE=arm-linux- ARCH=arm O=$2 all
+	;;
 	rnew) echo "start clean files, remake uboot, then download it into nand flash....."
 	rm -rf $2
 	make O=$2 clean
