6b72da21e3470082552436e039e73c7144470513
ilufei <dzxsk1988@gmail.com>
2015-10-26 22:46:45 +0800

    add support spl

--- include/configs/fl2440.h
+++ include/configs/fl2440.h
@@ -127,7 +127,6 @@
  */
 #define CONFIG_SYS_LONGHELP		/* undef to save memory */
 #define CONFIG_AUTO_COMPLETE
-#define CONFIG_SYS_PROMPT	"FL2440 # "
 #define CONFIG_SYS_CBSIZE	256
 /* Print Buffer Size */
 #define CONFIG_SYS_PBSIZE	(CONFIG_SYS_CBSIZE + \
